8c486962d607f826e9369fe91a279dcf2b8797ad
tianzhen <f17631387926@163.com>
2026-09-13 14:08:53 +0800

    fix settings heading

--- src/settings.ts
+++ src/settings.ts
@@ -73,7 +73,7 @@ export class TranslatorSettingTab
         // ================================
 
         new Setting(containerEl)
-			.setName('TianLex')
+			.setName('Dictionary')
 			.setHeading();
 
 
